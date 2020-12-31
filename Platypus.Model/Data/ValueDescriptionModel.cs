cf023df1d202d1b9b0ecc22199b37cf22b63a6e7
Jack Nicol <jack.nicol@insiso.co.uk>
2020-12-31 13:57:38 +0000

    added constant controller

---
+++ Platypus.Model/Data/ValueDescriptionModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Platypus.Model.Data {
+
+    public class ValueDescriptionModel<T1, T2> {
+        public T1 Value { get; set; }
+        public T2 Description { get; set; }
+    }
+}
---
+++ Platypus/Controllers/ConstantController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Platypus.Model.Constants;
+using Platypus.Model.Data;
+using Platypus.Model.Data.BankAccount;
+using System.Linq;
+
+namespace Platypus.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConstantContoller : ControllerBase {
+
+        [HttpGet("transactionCategories")]
+        [ProducesResponseType(200, Type = typeof(ValueDescriptionModel<string, string>))]
+        [ProducesResponseType(401)]
+        public IActionResult GetTransactionCategories([FromBody]BankAccountCreateModel model) {
+            var constants = TransactionCategoryConstant.ValuesAndDescriptions
+                .Select(x => new ValueDescriptionModel<string, string> {
+                    Value = x.Key,
+                    Description = x.Value
+                });
+
+            return Ok(constants);
+        }
+    }
+}
